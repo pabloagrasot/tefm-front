cd0c06a86cdad7732503d399d3ea0174a21692c2
Pablo <p.m.agrasot@hotmail.com>
2021-01-27 18:34:49 +0100

    cronometro cuenta atras funcionando

--- src/ejercicio/Ejercicios.tsx
+++ src/ejercicio/Ejercicios.tsx
@@ -1,9 +1,19 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 export const Ejercicios: FC = () => {
-  
-  return (<h1>Ejercicios</h1>
-        
+  const [seconds, setSeconds] = useState(60);
+   function UpdateTimer() {
+
+    window.setInterval(() => {
+     setSeconds( seconds => seconds - 1);
+    }, 1000)
+  }
+  return (
+    <>
+      <h1>Ejercicios</h1>
+      <h2>{seconds}</h2>
+      <button onClick={UpdateTimer} >Empezar</button>
+    </> 
 
    
   )
