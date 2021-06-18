8391e27111b53ae222bc1e2e0d9da40797d7a2c6
pabloagrasot <p.m.agrasot@hotmail.com>
2021-06-18 22:25:31 +0200

    conect to signup api

--- src/registro/Registro.tsx
+++ src/registro/Registro.tsx
@@ -1,7 +1,9 @@
-import { FC} from 'react'
-import { Formik, Form, Field, ErrorMessage, FormikHelpers } from 'formik';
+import { FC, useState} from 'react'
+import { Formik, useFormik, } from 'formik';
 import './registro.css';
 import * as FaIcons from "react-icons/fa";
+import * as yup from 'yup'
+import axios from 'axios';
 interface props{
   className:string
   changeClass: () => void
@@ -16,50 +18,49 @@ interface Values {
   confirmPassword:string;
 }
 
+
+const validationSchema = yup.object().shape({
+  name: yup.string().required('Campo requerido'),
+  lastName: yup.string().required('Campo requerido'),
+  email: yup.string().email('Introduce un email correcto').required('Campo requerido'),
+  userName:yup.string().required('Campo requerido'),
+  password: yup.string().required('Campo requerido')
+  .oneOf([yup.ref('confirmPassword')], 'Las contraseñas no son inguales'),
+  confirmPassword:yup.string().required('Campo requerido')
+  .oneOf([yup.ref('password')], 'Las contraseñas no son inguales')
+})
 const sleep = (ms:number) => new Promise((r) => setTimeout(r, ms));
 
-export const Registro: FC<props> = ({className, changeClass}) => {
 
-  //const [modal, setModal] = useState(false)
-  //const showModal = () => setModal(!modal);
+
+export const Registro: FC<props> = ({className, changeClass}) => {
+  const [success, setSuccess] = useState(null)
+
+  const onSubmit = async (values:Values) => {
+
+    const {confirmPassword, ...data} = values
+
+          const response = await axios.post('http://localhost:3500/signup', data).catch((err) => {
+            if (err && err.response)
+            console.log('Error: ', err);
+          })
+
+          if(response && response.data){
+            setSuccess(response.data.message)
+          }
+
+  }
+
+  const formik = useFormik({initialValues: { name: '', lastName: '', email: '', userName: '', password: '', confirmPassword: '' }, 
+    validateOnBlur:true,
+    onSubmit,
+    validationSchema: validationSchema
+  })
 
   return (
     <section className={className}>
-      <Formik
-       initialValues={{ name: '', lastName: '', email: '', userName: '', password: '', confirmPassword: '' }}
-       validate={values => {
-         const errors = {name: '', lastName: '', email: '', userName: '', password: '', confirmPassword: ''};
-         if (!values.name) {
-          errors.name = 'Requerido';
-        }else if (!values.lastName) {
-          errors.lastName = 'Requerido';
-        }else if (!values.email) {
-           errors.email = 'Requerido';
-          }else if (!values.userName) {
-            errors.userName = 'Requerido';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Dirección de correo incorecta';
-         } else if (!values.password){
-            errors.password = 'Requerido'
 
-         } else if (!values.confirmPassword){
-            errors.confirmPassword = 'Requerido'
-             
-         } else if (values.password !== values.confirmPassword){
-            errors.confirmPassword = 'Las contraseñas no coinciden'
-         }return errors;
-         
-       }}
-       onSubmit={async (values:Values) => {
-        await sleep(500);
-       console.log(values);
-      }}
-     >
-       {({ isSubmitting }) => (
-
-         <Form className='modal__form'>
+         <form className='modal__form' onSubmit={formik.handleSubmit} >
 
            <div className='close' onClick={changeClass}>
               <FaIcons.FaTimes />
@@ -69,42 +70,43 @@ export const Registro: FC<props> = ({className, changeClass}) => {
               <h2>Regístrate</h2>
             </div>
 
-            <div className='form-input'>
-              <Field id="name" placeholder='Nombre' className='input' type='text' name='name' />
-              <ErrorMessage className='input__error' name='name' component='div' />
+            <div>
+              <p className='form-success'> {success ? success: 'Usuario registrado'} </p>
             </div>
 
             <div className='form-input'>
-              <Field id='lastName' placeholder='Apellido' className='input' type='text' name='lastName' />
-              <ErrorMessage className='input__error' name='lastName' component='div' />
+              <input id="name" placeholder='Nombre' className='input' type='text' name='name' value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+              <div className='input__error'>{formik.touched.name && formik.errors.name ? formik.errors.name: ''}</div>
             </div>
 
             <div className='form-input'>
-              <Field id='email' placeholder='Correo electrónico' className='input' type='email' name='email' />
-              <ErrorMessage className='input__error' name='email' component='div' />
+              <input id='lastName' placeholder='Apellido' className='input' type='text' name='lastName' value={formik.values.lastName} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+              <div className='input__error'>{formik.touched.lastName && formik.errors.lastName ? formik.errors.lastName: ''}</div>
             </div>
 
             <div className='form-input'>
-              <Field id='userName' placeholder='Nombre de Usuario' className='input' type='text' name='userName' />
-              <ErrorMessage className='input__error' name='userName' component='div' />
+              <input id='email' placeholder='Correo electrónico' className='input' type='email' name='email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+              <div className='input__error'>{formik.touched.email && formik.errors.email ? formik.errors.email: ''}</div>
             </div>
 
             <div className='form-input'>
-              <Field id='password' placeholder='Contraseña' className='input' type='password' name='password' />
-              <ErrorMessage className='input__error' name='password' component='div' />
+              <input id='userName' placeholder='Nombre de Usuario' className='input' type='text' name='userName' value={formik.values.userName} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+              <div className='input__error'>{formik.touched.userName && formik.errors.userName ? formik.errors.userName: ''}</div>
+            </div>
+
+            <div className='form-input'>
+              <input id='password' placeholder='Contraseña' className='input' type='password' name='password' value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+              <div className='input__error'>{formik.touched.password && formik.errors.password ? formik.errors.password: ''}</div>
            </div>
 
            <div className='form-input'>
-              <Field id='confirmPassword' placeholder='Confirmar contraseña' className='input' type='password' name='confirmPassword' />
-              <ErrorMessage className='input__error' name='confirmPassword' component='div' />
+              <input id='confirmPassword' placeholder='Confirmar contraseña' className='input' type='password' name='confirmPassword' value={formik.values.confirmPassword} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+              <div className='input__error'>{formik.touched.confirmPassword && formik.errors.confirmPassword ? formik.errors.confirmPassword: ''}</div>
            </div>
 
-           <button className='primary-button' type='submit' disabled={isSubmitting}>Registrarme</button>
+           <button className='primary-button' type='submit'>Registrarme</button>
            
-         </Form>
-
-       )}
-     </Formik>
+         </form>
       
     </section> 
   )
