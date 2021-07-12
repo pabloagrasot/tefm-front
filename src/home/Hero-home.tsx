fe83537b3d8f2e9fb26b151207b585ccaf409656
pabloagrasot <p.m.agrasot@hotmail.com>
2021-07-12 22:19:40 +0200

    home

--- src/home/Hero-home.tsx
+++ src/home/Hero-home.tsx
@@ -1,15 +1,16 @@
 import { FC,} from 'react'
 import './home.css';
-import hero from '../img/hero.svg'
+import hero from '../img/hero-2.svg'
 
 export const Hero : FC = () => {
 
   return (
-    <>
-       <section className='hero'>
-       <img src={hero} alt="" className="" />
+      <section className='hero__section'>
+        <div className='hero'>
+          <img src={hero} />
+          <h2>La herramienta complementaria para tu circuito de Educación Física por estaciones</h2>
+        </div> 
+      </section>
 
-       </section>
-    </>
   )
 }
--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -6,7 +6,16 @@ export const Home : FC = () => {
 
   return (
     <>
-       <Hero/>  
+       <Hero/>
+
+       <section className='home-body__section'>
+
+          <div>
+            <h2>¿Qué es Training <span>TEA</span></h2>
+            <p>Training TEA es una web cuyo objetivo es dar apoyo en las sesiones de educación física con alumnos TEA. Para fomentar el ejercicio y poder realizar ejercicos </p>
+          </div>
+
+       </section>
     </>
   )
 }
